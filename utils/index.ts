c41e35daee4355d7664e3171e9faec11cd4af093
necelentano <necelentano@gmail.com>
2022-11-01 22:13:47 +0200

    Added Category Name in the Head

--- utils/index.ts
+++ utils/index.ts
@@ -8,3 +8,14 @@ export const formatDate = (dateString: string): string => {
 
   return date;
 };
+
+export const makeCategory = (slug: string): string => {
+  if (typeof slug === "string") {
+    return slug.split("-").join(" ");
+  }
+  return "";
+};
+
+export const capitalyzeFirstLetter = (str: string): string => {
+  return `${str.charAt(0).toLocaleUpperCase()}${str.slice(1)}`;
+};
